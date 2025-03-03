78149c810f4fe01fb048ad3ce0da1b8e50a66611
BenjaminTSmith <smithbenjamin@ufl.edu>
2025-03-03 11:45:07 -0500

    add axios and mysql to flask

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
+from flaskext.mysql import MySQL
 import time
 
 app = Flask(__name__)
+CORS(app) # allows cross origin communication with React
 
+# MySQL Database Connection
+mysql = MySQL(app, user="", password="", db="") # TODO: James or Oliver add mySQL settings or change db
+
+# test request
 @app.route("/time")
 def get_time():
     return { "time": time.time() }
+
+
+@app.route("/tutors")
+def get_tutors():
+    return {}
