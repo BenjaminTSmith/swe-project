70fe42c6ad8b8b26dc401f813b2f1b7189f3b71e
BenjaminTSmith <smithbenjamin@ufl.edu>
2025-04-19 14:17:57 -0400

    added email sending for backend and environment variables for security

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,41 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flaskext.mysql import MySQL
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+import random
 
 app = Flask(__name__)
-CORS(app) # allows cross origin communication with React
+CORS(app) # allows communication with React
 
-# MySQL Database Connection
-mysql = MySQL(app, user="", password="", db="") # TODO: James or Oliver add mySQL settings or change db
+app.config.update(
+    MAIL_SERVER="smtp.gmail.com",
+    MAIL_PORT=587,
+    MAIL_USE_TLS=True,
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_DEFAULT_SENDER=os.getenv("MAIL_DEFAULT_SENDER"),
+)
+mail = Mail(app)
 
-# test request
-@app.route("/time", methods=["POST"])
-def time():
-    data = request.json
-    time = data.get("time", "No time sent");
-    print(time)
-    return jsonify({ "response": "Time received" })
+@app.route("/send_verification_email", methods=["POST"])
+def send_verification_email():
+    data = request.get_json()
+    user_email = data.get("email")
 
+    verification_code = f"{random.randint(000000, 999999):06d}"
 
-@app.route("/tutors")
-def get_tutors():
-    return {}
+    message = Message(
+        subject="Your Verification Code",
+        recipients=[user_email],
+        body=(
+            f"Hello,\n\n"
+            f"Your verification code for Tutors4UF is: {verification_code}\n\n"
+        )
+    )
+    try:
+        mail.send(message)
+    except Exception as e:
+        return jsonify({ "error": "Failed to send email" }), 500
+
+    return jsonify({ "code": verification_code }), 200
